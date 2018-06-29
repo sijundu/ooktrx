28ce35395bd18e8e81ae8cbfe1d3bb6da9e069d4
sijundu <sijundu@hotmail.com>
2018-06-29 16:02:10 -0700

    initial commit

--- test_run_dir/frame_sync_first_test/ooktrx.FrameSyncSpec945290574/VFrameSync__ALLsup.d
+++
@@ -1,6 +0,0 @@
-VFrameSync__ALLsup.o: VFrameSync__ALLsup.cpp VFrameSync.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilatedos.h VFrameSync__Trace.cpp \
- /usr/local/share/verilator/include/verilated_vcd_c.h VFrameSync__Syms.h \
- VFrameSync.h VFrameSync__Syms.cpp VFrameSync__Trace__Slow.cpp
