e50e18ee82cc1bf88334c4bedbc9e0f804ab20a3
Sijun Du <33942226+sijundu@users.noreply.github.com>
2018-06-29 15:52:33 -0700

    Add files via upload

---
+++ src/main/scala/ooktrx/FrameSync.scala
@@ -0,0 +1,38 @@
+// See README.md for licence details.
+
+package ooktrx
+
+import chisel3._
+import chisel3.util._
+
+
+/*
+case class FrameSyncParams (
+  frameBitsWidth: Int
+) {
+  require(frameBitsWidth > 0, s"Frame Bits Width must be > 2, got $frameBitsWidth")
+}
+*/
+
+
+class FrameSync (val frameBitsWidth: Int) extends Module {
+
+  require(frameBitsWidth > 0, s"Frame Bits Width must be > 2, got $frameBitsWidth")
+
+  val io = IO(new Bundle{
+    val in = Input(Bool())
+    val frameBits = Input(UInt(frameBitsWidth.W))
+    val syncOk = Output(Bool())
+  })
+
+  val fifoReg = RegInit(0.U(frameBitsWidth.W))
+
+  fifoReg := (fifoReg << 1) | io.in
+  io.syncOk := false.B
+
+  when (fifoReg === io.frameBits){
+    io.syncOk := true.B
+  }.otherwise {
+    io.syncOk := false.B
+  }
+}
---
+++ src/test/scala/ooktrx/FrameSyncSpec.scala
@@ -0,0 +1,45 @@
+// See README.md for license details.
+//
+package ooktrx
+
+import dsptools.DspTester
+import org.scalatest.{FreeSpec, Matchers}
+import chisel3._
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+import scala.util.Random
+
+class FrameSyncTester(val c: FrameSync) extends DspTester(c) {
+
+  val frameBits = "b0101".asUInt(c.frameBitsWidth.W)
+  var numberOfSteps = 0
+  var frameValidCounter = 0
+  var randomFrameBits = Random.nextInt(2)
+
+  poke(c.io.frameBits, frameBits)
+
+  while(frameValidCounter < 3 && numberOfSteps < 1000){
+    randomFrameBits = Random.nextInt(2)
+    poke(c.io.in, randomFrameBits != 0)
+    step(1)
+    when(c.io.syncOk){
+      frameValidCounter += 1
+    }
+    numberOfSteps += 1
+  }
+}
+
+class FrameSyncSpec extends FreeSpec with Matchers {
+  "First test: with 4-bit frame bits" in{
+    val frameBitsWidthNb = 4
+    val gen = () => new FrameSync(frameBitsWidthNb)
+    dsptools.Driver.execute(
+      gen, Array(
+        "--backend-name", "verilator",
+        "--target-dir", s"test_run_dir/frame_sync_first_test"
+      )
+    ) { c => 
+      new FrameSyncTester(c)
+    }
+  }
+}
+
