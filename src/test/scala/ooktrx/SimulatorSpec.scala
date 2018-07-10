a9d626c7ff70d9f54a56e372ffb04709d80584ee
sijundu <sijundu@hotmail.com>
2018-07-09 22:05:25 -0700

    update

--- src/test/scala/ooktrx/SimulatorSpec.scala
+++ src/test/scala/ooktrx/SimulatorSpec.scala
@@ -12,17 +12,18 @@ import scala.util.Random
 class SimulatorRandomInputTester(val c: Simulator) extends DspTester(c) {
 
   val frameBits = "b1111".asUInt(c.frameBitsWidth.W)
-  val divisor = "b1101".asUInt(c.divisorWidth.W)
+  val divisor = "b10101".asUInt(c.divisorWidth.W)
   var numberOfSteps = 0
-  var randomFrameBits = Random.nextInt(2)
-  var frameNb = 20
+  var startStepNb = 100
+  var frameNb = 100
 
   poke(c.io.frameBits, frameBits)
   poke(c.io.divisor, divisor)
 
-  while(numberOfSteps < 2000){
-    if((numberOfSteps >= 100) && (numberOfSteps < (100+frameNb))){
-      poke(c.io.dataIn, Random.nextInt(math.pow(2, (c.frameIndexWidth+c.dataWidth)).toInt).asUInt)
+  while(numberOfSteps < 5000){
+    if((numberOfSteps > startStepNb) && (numberOfSteps <= (startStepNb+frameNb))){
+      poke(c.io.dataIn, (((numberOfSteps-startStepNb) << c.dataWidth)+(Random.nextInt(math.pow(2, (c.dataWidth.toLong)).toInt))).asUInt)
+      //poke(c.io.dataIn, Random.nextInt(math.pow(2, (c.frameIndexWidth+c.dataWidth)).toLong).asUInt)
       poke(c.io.dataInValid, true.B)
       poke(c.io.txStart, true.B)
       poke(c.io.frameCount, frameNb.U)
@@ -32,7 +33,7 @@ class SimulatorRandomInputTester(val c: Simulator) extends DspTester(c) {
     //  poke(c.io.txStart, true.B)
     //  poke(c.io.frameCount, 18.U)
     }else{
-      poke(c.io.dataIn, 0.U(c.dataWidth.W))
+      poke(c.io.dataIn, 0.U((c.frameIndexWidth + c.dataWidth).W))
       poke(c.io.dataInValid, false.B)
       poke(c.io.txStart, false.B)
       poke(c.io.frameCount, 0.U)
@@ -48,13 +49,13 @@ class SimulatorSpec extends FreeSpec with Matchers {
   "RX Control test with random input bits" in{
     val gen = () => new Simulator(frameWidth = 32,
                               frameBitsWidth = 4,
-                              frameIndexWidth = 4,
+                              frameIndexWidth = 8,
                               dataWidth = 16,
-                              divisorWidth = 9,
-                              txStackSize =16,
-                              txMemSize = 32, 
-                              rxStackSize =16,
-                              rxMemSize = 32 )
+                              divisorWidth = 5,
+                              txStackSize = 64,
+                              txMemSize = 128, 
+                              rxStackSize = 8,
+                              rxMemSize = 128 )
     dsptools.Driver.execute(
       gen, Array(
         "--backend-name", "verilator",
