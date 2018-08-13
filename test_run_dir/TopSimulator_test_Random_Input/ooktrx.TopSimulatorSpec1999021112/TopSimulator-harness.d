963c84c565865fb75092668a7fd34cbafaf0d263
Sijun Du <sijun@berkeley.edu>
2018-08-13 11:12:07 -0700

    updates

--- test_run_dir/TopSimulator_test_Random_Input/ooktrx.TopSimulatorSpec1999021112/TopSimulator-harness.d
+++
@@ -1,6 +0,0 @@
-TopSimulator-harness.o: \
-  /Users/sijun/Documents/work/projects/ooktrx/test_run_dir/TopSimulator_test_Random_Input/ooktrx.TopSimulatorSpec1999021112/TopSimulator-harness.cpp \
-  VTopSimulator.h /usr/local/share/verilator/include/verilated.h \
-  /usr/local/share/verilator/include/verilated_config.h \
-  /usr/local/share/verilator/include/verilatedos.h veri_api.h sim_api.h \
-  /usr/local/share/verilator/include/verilated_vcd_c.h
--- test_run_dir/TopSimulator_test_Random_Input/ooktrx.TopSimulatorSpec1999021112/VTopSimulator__ALLcls.d
+++
@@ -1,5 +0,0 @@
-VTopSimulator__ALLcls.o: VTopSimulator__ALLcls.cpp VTopSimulator.h \
-  /usr/local/share/verilator/include/verilated.h \
-  /usr/local/share/verilator/include/verilated_config.h \
-  /usr/local/share/verilator/include/verilatedos.h VTopSimulator.cpp \
-  VTopSimulator__Syms.h
